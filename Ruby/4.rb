f8f77e85c0daddfcb3b6e3c51178f69fd1cc08ab
mattetress <mattetress@me.com>
2021-07-16 14:52:01 -0500

    add bubble sort challenge

---
+++ Ruby/4.rb
@@ -0,0 +1,12 @@
+require 'rspec'
+
+# Bubble Sort Algorithm
+
+
+# Tests
+describe "Adding bubble sort to the Array class" do
+  it "Properly sorts an array" do
+    arr = [4, 1, 6, 10, 44, 2, 3]
+    expect(arr.bubble_sort).to eq(arr.sort)
+  end
+end
